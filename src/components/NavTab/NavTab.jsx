8e8e38cf41a823b77c3b21d2ba962d01fb7d4179
Daria Rutts <darya.rutts@yandex.ru>
2023-09-05 21:15:44 +0300

    refactor: изменить тег больших секций

--- src/components/NavTab/NavTab.jsx
+++ src/components/NavTab/NavTab.jsx
@@ -3,11 +3,11 @@ import './NavTab.css';
 
 const NavTab = () => {
   return (
-    <div className="navtab">
+    <section className="navtab">
       <a href="#aboutProject" className="navtab__link">О проекте</a>
       <a href="#techs" className="navtab__link">Технологии</a>
       <a href="#aboutMe" className="navtab__link">Студент</a>
-    </div>
+    </section>
   )
 };
 
--- src/components/Promo/Promo.jsx
+++ src/components/Promo/Promo.jsx
@@ -4,10 +4,10 @@ import promoLogo from "../../images/landing-logo.png";
 
 const Promo = () => {
   return (
-    <div className="promo">
+    <section className="promo">
       <img src={promoLogo} alt="Изображение буквы П на клетчатом фоне с кругом" className="promo__image"/>
       <h1 className="promo__heading">Учебный проект студента факультета Веб-разработки.</h1>
-    </div>
+    </section>
   )
 };
 
